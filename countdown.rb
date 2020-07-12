a0f4222264887bee032425ebb08367351bf45fac
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-12 18:28:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+number = 0 
+def countdown(number)
+  while number += 1 
+  puts "#{number} SECOND(S)!"
+  until number -= 1 
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+
 end
